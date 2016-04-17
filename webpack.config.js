b44946568d1f038880747655ce7c97b179d4e3dd
xiilei <xilei125@163.com>
2016-04-17 23:05:05 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    context:__dirname+'/public',
+    entry:{
+        'vendor':['jquery','bootstrap','react','react-dom'],
+        'index':'./entry'
+    },
+    output:{
+            filename: '[name]-bundle.js',
+            path: __dirname+'/public/build',
+            publicPath:'/assets/build/'
+    },
+    resolve:{
+        extensions:["", ".jsx", ".js"]
+    },
+    module: {
+      loaders: [
+        { test: /\.jsx?$/,exclude: /node_modules/,loader: 'babel'},
+        { test: /\.css$/, loader: "css"},
+        { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") },
+        { test: /\.(eot|woff|woff2|ttf|svg)$/, loader: "file-loader" }
+      ]
+    },
+    plugins: [
+        new ExtractTextPlugin("[name]-bundle.css")
+    ]
+};
